f29ac4eac1e50a2bf7ea1a2e826dd8506dafa2d4
Mike Davis <mike@michaeljacobdavis.com>
2015-12-20 14:22:04 -0800

    Add logging

--- main.js
+++ main.js
@@ -2,26 +2,28 @@
 
 const electron = require('electron');
 const path = require('path');
-const GhReleases = require('electron-gh-releases');
 const WindowHandler = require('./main/window-handler');
 const tick = require('./main/tick');
 const runner = require('./main/runner');
+const update = require('./main/update');
+const logger = require('./main/logger');
 const appActions = require('./common/action-types/app');
 const bus = require('./main/event-bus');
 const notifier = require('node-notifier');
-const pkg = require('./package.json');
 const app = electron.app;
 const Tray = electron.Tray;
-const dialog = electron.dialog;
 const crashReporter = electron.crashReporter;
 const ipc = electron.ipcMain;
-const updater = new GhReleases({
-  repo: pkg.repo,
-  currentVersion: app.getVersion()
-});
 
 require('electron-debug')();
 crashReporter.start();
+
+process.once('uncaughtException', (error) => {
+  logger.error('uncaughtException', { message: error.message, stack: error.stack });
+
+  // Exit with error
+  process.exit(1);
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
@@ -54,8 +56,6 @@ app.on('ready', () => {
     notifier.notify(notification);
   });
 
-  runner(ipc, tick);
-
   function click(e, bounds) {
     if (e.altKey || e.shiftKey || e.ctrlKey || e.metaKey) {
       return windowHandler.hide();
@@ -79,42 +79,6 @@ app.on('ready', () => {
     app.dock.hide();
   }
 
-  checkAutoUpdate(false);
-
-  function checkAutoUpdate(showAlert) {
-
-    // Check for updates
-    updater.check((err, status) => {
-      console.log(err)
-      console.log(status)
-      if (!err && status) {
-        updater.download();
-      } else {
-        if (showAlert) {
-          dialog.showMessageBox({
-            type: 'info',
-            buttons: ['Close'],
-            title: 'No update available',
-            message: 'You are currently running the latest version of Pomo.'
-          });
-        }
-      }
-    });
-
-    updater.on('update-downloaded', (info) => {
-      console.log(info)
-      dialog.showMessageBox({
-        type: 'question',
-        buttons: ['Update & Restart', 'Cancel'],
-        title: 'Update Available',
-        cancelId: 99,
-        message: 'There is an update available. Would you like to update Pomo now?'
-      }, (response) => {
-        console.log('Exit: ' + response);
-        if (response === 0) {
-          updater.install()
-        }
-      });
-    })
-  }
+  runner(ipc, tick);
+  update(false);
 });
---
+++ main/update.js
@@ -0,0 +1,46 @@
+const GhReleases = require('electron-gh-releases');
+const electron = require('electron');
+const pkg = require('../package.json');
+const logger = require('./logger');
+
+const app = electron.app;
+const dialog = electron.dialog;
+
+const updater = new GhReleases({
+  repo: pkg.repo,
+  currentVersion: app.getVersion()
+});
+
+module.exports = function update(showAlert) {
+
+  // Check for updates
+  updater.check((err, status) => {
+    if (!err && status) {
+      updater.download();
+    } else {
+      if (showAlert) {
+        dialog.showMessageBox({
+          type: 'info',
+          buttons: ['Close'],
+          title: 'No update available',
+          message: 'You are currently running the latest version of Pomo.'
+        });
+      }
+    }
+  });
+
+  updater.on('update-downloaded', (info) => {
+    dialog.showMessageBox({
+      type: 'question',
+      buttons: ['Update & Restart', 'Cancel'],
+      title: 'Update Available',
+      cancelId: 99,
+      message: 'There is an update available. Would you like to update Pomo now?'
+    }, (response) => {
+      console.log('Exit: ' + response);
+      if (response === 0) {
+        updater.install()
+      }
+    });
+  })
+}
