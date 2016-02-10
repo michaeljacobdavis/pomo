9e692d48442deda8926e748ec4e809c24e65979b
Mike Davis <mike@michaeljacobdavis.com>
2016-02-09 20:20:01 -0800

    Travis

--- scripts/release.bash
+++ scripts/release.bash
@@ -3,9 +3,12 @@
 set -e
 
 rm -rf ./release
+npm install
 npm version minor
 npm run package
 npm run sign
 electron-release --token=$TOKEN --app=release/darwin-x64/Pomo-darwin-x64/Pomo.app
 git add auto_updater.json
 git commit --amend --no-edit
+git push
+git push origin --tags
